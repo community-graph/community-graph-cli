18eec3b20b3e14646bc389e5aac397d30f278557
Mark Needham <mark.needham@neotechnology.com>
2017-10-03 08:51:32 +0100

    move the init out

--- src/index.js
+++ src/index.js
@@ -201,31 +201,7 @@ if (command == null) {
             console.error("Error updating community graph:", err);
             process.exit(1);
         });
-    } else if(command == "init") {
-        let welcome = new Promise((resolve, reject) => {
-            console.log("Initialising the community graph");
-            resolve();
-        });
-
-        welcome
-        .then(prereqs.checkPythonVersion)
-        .then(prereqs.removePyCache).then(data => {
-            const serverless = new Serverless({});
-            const CLI = require('serverless/lib/classes/CLI');
-
-            CLI.prototype.processInput = function () {
-                return { commands: ['invoke'], options: { help: false, function: "constraints" } };
-            };
-
-            serverless.cli = CLI;
-            return serverless.init().then(() => serverless.run());
-        }).then(data => {
-            console.log("Community graph initialised.");
-        }).catch(err => {
-            console.error("Error updating community graph:", err);
-            process.exit(1);
-        });
-    } else if (command == "create-neo4j-server") {
+    }  else if (command == "create-neo4j-server") {
         console.log("Creating a Neo4j server");
 
         let args = parseArgs(argv);
