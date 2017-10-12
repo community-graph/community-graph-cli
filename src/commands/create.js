56e7a789e2108bc7c6e5250842b7cb8cd1b26d00
Mark Needham <mark.needham@neotechnology.com>
2017-10-12 11:59:05 +0100

    create as its own commnad

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,8 +1,92 @@
 const prereqs = require("../prereqs");
 const Serverless = require('serverless');
 const program = require('commander');
+const cli = require("../cli");
+const AWS = require("aws-sdk");
+const fs = require('fs');
 
 program.parse(process.argv);
+
+let rawParams = {};
+let communityGraphParams = {
+    credentials: {
+        write: {},
+        readonly: {}
+    }
+};
+
+let regionParams = { 'region': 'us-east-1' }
+let kms = new AWS.KMS(regionParams);
+let s3 = new AWS.S3(regionParams);
+var ec2 = new AWS.EC2(regionParams);
+
+function _createKMSKeyAlias(communityName, kmsKeyArn) {
+    let createAliasParams = {
+        AliasName: "alias/CommunityGraphCLI-" + communityName,
+        TargetKeyId: kmsKeyArn
+    };
+
+    return kms.createAlias(createAliasParams).promise();
+}
+
+function _createKMSKey() {
+    return kms.createKey({}).promise();
+}
+
+function _createS3Bucket(s3BucketName) {
+    console.log("Creating bucket: " + s3BucketName);
+    var params = { Bucket: s3BucketName, ACL: "public-read" };
+    return s3.createBucket(params).promise()
+}
+
+function encryptKey(data, keyName, mapToUpdate) {
+    let kmsKey = data.kmsKeyArn;
+    let valueToEncrypt = data[keyName];
+
+    console.log("Encrypting " + keyName + ":" + valueToEncrypt);
+
+    return new Promise((resolve, reject) => {
+        if (!valueToEncrypt) {
+            console.log(keyName + " not provided - skipping");
+            resolve(data);
+        } else {
+            let params = { KeyId: kmsKey, Plaintext: valueToEncrypt };
+
+            kms.encrypt(params).promise().then(result => {
+                mapToUpdate[keyName] = result.CiphertextBlob.toString('base64');
+                resolve(data);
+            }).catch(reject);
+        }
+    });
+}
+
+function writeCommunityGraphJson(data) {
+    communityGraphParams.communityName = data.communityName;
+    communityGraphParams.tag = data.tag;
+    communityGraphParams.serverUrl = data.serverUrl;
+    communityGraphParams.logo = data.logo;
+    communityGraphParams.s3Bucket = data.s3Bucket;
+    communityGraphParams.twitterSearch = data.twitterSearch;
+
+    communityGraphParams.credentials.keyArn = data.kmsKeyArn;
+
+    communityGraphParams.credentials.readonly.user = data.readOnlyServerUsername || "<read-only-username>";
+    communityGraphParams.credentials.readonly.password = communityGraphParams.credentials.readonly.readOnlyServerPassword ||  "<read-only-password>";
+    communityGraphParams.credentials.write.user = data.serverUsername || "neo4j";
+    communityGraphParams.credentials.write.password = communityGraphParams.credentials.write.serverPassword || "<server-password>";
+
+    delete communityGraphParams.credentials.readonly.readOnlyServerPassword;
+    delete communityGraphParams.credentials.write.serverPassword;
+
+    return new Promise((resolve, reject) => {
+        try {
+            fs.writeFileSync("communitygraph.json", JSON.stringify(communityGraphParams, null, 4));
+            resolve(data);
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
 
 let welcome = new Promise((resolve, reject) => {
     console.log("Welcome to the community graph - it's time to find out what's happening in your community!");
@@ -73,6 +157,8 @@ welcome
     }
 }).then(data => {
     return writeCommunityGraphJson(data);
+}).then(data => {
+    console.log(`Community Graph created! If you want to update any of the parameters edit communitygraph.json`)
 }).catch(err => {
     console.error("Error while creating community graph:", err);
     process.exit(1);
